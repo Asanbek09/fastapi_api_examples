ced3b23c44b10cd418f39ab91482b9613632d85e
Asanbek09 <samuidnua@gmail.com>
2024-09-06 10:30:57 +0600

    continue ...

--- Chapter_01/01/model.py
+++ Chapter_01/01/model.py
@@ -4,6 +4,50 @@ class Todo(BaseModel):
     id: int
     item: str
 
+    class Config:
+        json_schema_extra = {
+            "Example": {
+                "id": 1,
+                "item": "Example schema!"
+            }
+        }
+
+class TodoItem(BaseModel):
+    item: str
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "item": "Read the next chapter of the book"
+            }
+        }
+
+class TodoItems(BaseModel):
+    todos: list[TodoItem]
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "todos": [
+                    {
+                        "item": "Example schema 1"
+                    },
+                    {
+                        "item": "Example schema 2"
+                    }
+                ]
+            }
+        }
+
 class Item(BaseModel):
     item: str
     status: str
+
+    class Config:
+        json_schema_extra = {
+            "Example": {
+                "item": "Example 2",
+                "status": "Status text"
+            }
+        }
+
